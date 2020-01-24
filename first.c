8ce81131772f4dcf7559dfa7b2f183c1c9a9168f
vi-hseng <vigneshashok@mail.usf.edu>
2020-01-24 16:37:56 -0500

    Initial commit

---
+++ first.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h> //standard input/output library
+
+main() // define a function main that receives no argument values
+{ // statements of main are enclosed in braces
+	printf("hello, world\n");
+} //main calls library function printf to print the sequence of characters; \n represents the newline character
+
+
+
